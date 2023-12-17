2a6fd3677d18d11dee4ddd91cf18ad40eb535342
Nikol-0207 <pwendynicol@gmail.com>
2023-12-16 20:40:36 -0400

    tester3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+function reset(){
+ var textarea = document.getElementById('textoarea').value='';
+ textarea.value ='';
+    
+}
+
+
+
+
+
+
