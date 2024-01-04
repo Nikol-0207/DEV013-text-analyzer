07fd3cb603ef7ebbd0b059f6c65b22f90e9d7fbe
Nikol-0207 <pwendynicol@gmail.com>
2024-01-04 19:20:09 -0400

    tester8

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,18 +1,35 @@
 const analyzer = {  
+
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  //  const palabras = textarea.value.trim();
-      let arreglotext = text.split();
-      console.log(arreglotext);
-     
+      let arreglotext = text.split(/\s+/);
+       let contador=0;
+       for (var i = 0; i < arreglotext.length; i++) {
+          contador++;
+       }
+     return contador;
   },
+  
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
-  
+      return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
-  
+    const nuevotext = text.replace(/[^\w\s]|_/g,"");
+    let contador = 0;
+    for (var i = 0; i < nuevotext.length; i++) {
+      if (!nuevotext[i] == ' '){
+         contador++; 
+      }
+    } 
+    return contador;
+    
+    /*for (var i = 0; i < text.length; i++ ){
+       if (text[i] =' ') || (text[i] = '¿') || (text[i] ='?'){
+       }
+    } */
+
   },
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import analyzer from './analyzer.js';
-const textarea = document.getElementById('textoarea');
-    const words = document.getElementById('numbers');
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-function limpiar(){
-   let limpia = document.getElementById('textoarea');
-   limpia.value ='';   
-}
-function lecturaDdatos(){
-   analyzer.getWordCount();
 
-}
+const textarea = document.getElementById('textoarea');   //contenido del textarea
+let valorDelSpan = document.getElementById('numbers').innerHTML;  //span de resultados
+let contPal = analyzer.getWordCount(textarea);
+
+console.log(textarea);
+//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+/*function limpiar(){ 
+}*/
+ function contadorDePalabras(){
+
+ }
+
+
 
 
    
